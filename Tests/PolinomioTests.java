bc2c7f4a171e6e8d3bb606684a02996220625823
Brian Servis <brian_servis@yahoo.com>
2018-05-07 06:17:23 -0300

    Todos los métodos punto 1 menos Recursivas
    Junté lo que hice con los métodos de Daniel y agregué los tests. Faltan los métodos recursivos. Decidir si hacer un Main o tests

--- Tests/PolinomioTests.java
+++ Tests/PolinomioTests.java
@@ -4,36 +4,233 @@ import io.Polinomio;
 public class PolinomioTests {
 	
 	@Test
-	public void multicacionSucesiva() {
-		double[]coeficientes = {2,3,5,0};
+	public void multicacionSucesivaGrado3() {
+		double[]coeficientes = {2,-3,5,0};
 		Polinomio polinomio = new Polinomio(3, coeficientes);
-		System.out.println("sucesiva" + polinomio.evaluarMSucesivas(3));
-
-		Assert.assertEquals(96, polinomio.evaluarMSucesivas(3), 0);
-	}
-
-	@Test
-	public void recursividad() {
-		double[]coeficientes = {2,3,5,0};
-		Polinomio polinomio = new Polinomio(3, coeficientes);
-		System.out.println("recursiva" + polinomio.evaluarRecursiva(3));
-		Assert.assertEquals(96,  polinomio.evaluarRecursiva(3) , 0);
+		//System.out.println("Multiplicacion Sucesiva Grado 3 " + polinomio.evaluarMSucesivas(3));
+		Assert.assertEquals(42, polinomio.evaluarMSucesivas(3), 0);
 	}
 	
 	@Test
-	public void programacionDinamica() {
-		double[]coeficientes = {2,3,5,0};
-		Polinomio polinomio = new Polinomio(3, coeficientes);
-		System.out.println("ProgDinamica" + polinomio.evaluarProgDinamica(3));
-		Assert.assertEquals(96, polinomio.evaluarProgDinamica(3), 0);
+	public void multicacionSucesivaGrado6() {
+		double[]coeficientes = {-5,3,0,-2,7,3,0};
+		Polinomio polinomio = new Polinomio(6, coeficientes);
+		//System.out.println("Multiplicacion Sucesiva Grado 6 " + polinomio.evaluarMSucesivas(3));
+		Assert.assertEquals(-2898, polinomio.evaluarMSucesivas(3), 0);
 	}
 	
 	@Test
-	public void evaluarHorner() {
-		double[]coeficientes = {2,3,5,0};
+	public void multicacionSucesivaGrado0() {
+		double[]coeficientes = {5};
+		Polinomio polinomio = new Polinomio(0, coeficientes);
+		//System.out.println("Multiplicacion Sucesiva Grado 0 " + polinomio.evaluarMSucesivas(5));
+
+		Assert.assertEquals(5, polinomio.evaluarMSucesivas(5), 0);
+	}
+	
+	@Test
+	public void multicacionSucesivaGrado1() {
+		double[]coeficientes = {3,5};
+		Polinomio polinomio = new Polinomio(1, coeficientes);
+		//System.out.println("Multiplicacion Sucesiva Grado 1 " + polinomio.evaluarMSucesivas(2));
+
+		Assert.assertEquals(11, polinomio.evaluarMSucesivas(2), 0);
+	}
+
+//	@Test
+//	public void recursivoGrado3() {
+//		double[]coeficientes = {2,-3,5,0};
+//		Polinomio polinomio = new Polinomio(3, coeficientes);
+//		System.out.println("Recursiva Grado 3 " + polinomio.evaluarRecursiva(3));
+//		Assert.assertEquals(42,  polinomio.evaluarRecursiva(3) , 0);
+//	}
+//
+//
+//	@Test
+//	public void recursivoGrado6() {
+//		double[]coeficientes = {-5,3,0,-2,7,3,0};
+//		Polinomio polinomio = new Polinomio(6, coeficientes);
+//		//System.out.println("Multiplicacion Sucesiva Grado 6 " + polinomio.evaluarRecursiva(3));
+//		Assert.assertEquals(-2898, polinomio.evaluarRecursiva(3), 0);
+//	}	
+//	@Test
+//	public void recursivoGrado0() {
+//		double[]coeficientes = {2};
+//		Polinomio polinomio = new Polinomio(0, coeficientes);
+//		System.out.println("Recursiva Grado 0 " + polinomio.evaluarRecursiva(2));
+//		Assert.assertEquals(2, polinomio.evaluarRecursiva(2), 0);
+//	}
+//	
+//	@Test
+//	public void recursivoGrado1() {
+//		double[]coeficientes = {2,3};
+//		Polinomio polinomio = new Polinomio(1, coeficientes);
+//		System.out.println("Recursiva Grado 1 " + polinomio.evaluarRecursiva(2));
+//		Assert.assertEquals(7, polinomio.evaluarRecursiva(2), 0);
+//	}
+//
+//
+//	@Test
+//	public void recursivoParImparGrado3() {
+//		double[]coeficientes = {2,-3,5,0};
+//		Polinomio polinomio = new Polinomio(3, coeficientes);
+//		System.out.println("Recursiva Par o Impar Grado 3 " + polinomio.evaluarRecursivaParImpar(3));
+//		Assert.assertEquals(42,  polinomio.evaluarRecursivaParImpar(3) , 0);
+//	}
+//
+//	@Test
+//	public void recursivoParImparGrado6() {
+//		double[]coeficientes = {-5,3,0,-2,7,3,0};
+//		Polinomio polinomio = new Polinomio(6, coeficientes);
+//		//System.out.println("Multiplicacion Sucesiva Grado 6 " + polinomio.evaluarRecursivaParImpar(3));
+//		Assert.assertEquals(-2898, polinomio.evaluarRecursivaParImpar(3), 0);
+//	}	
+//
+//	@Test
+//	public void recursivoParImparGrado0() {
+//		double[]coeficientes = {2};
+//		Polinomio polinomio = new Polinomio(0, coeficientes);
+//		System.out.println("Recursiva Par o Impar Grado 0 " + polinomio.evaluarRecursivaParImpar(2));
+//		Assert.assertEquals(2, polinomio.evaluarRecursivaParImpar(2), 0);
+//	}
+//	
+//	@Test
+//	public void recursivoParImparGrado1() {
+//		double[]coeficientes = {2,3};
+//		Polinomio polinomio = new Polinomio(1, coeficientes);
+//		System.out.println("Recursiva Par o Impar Grado 1 " + polinomio.evaluarRecursivaParImpar(2));
+//		Assert.assertEquals(7, polinomio.evaluarRecursivaParImpar(2), 0);
+//	}	
+	
+	
+	@Test
+	public void programacionDinamicaGrado3() {
+		double[]coeficientes = {2,-3,5,0};
 		Polinomio polinomio = new Polinomio(3, coeficientes);
-		System.out.println("Horner" + polinomio.evaluarHorner(3));
-		Assert.assertEquals(96, polinomio.evaluarHorner(3), 0);
+		//System.out.println("Programacion Dinamica Grado 3 " + polinomio.evaluarProgDinamica(3));
+		Assert.assertEquals(42, polinomio.evaluarProgDinamica(3), 0);
+	}
+
+	@Test
+	public void programacionDinamicaGrado6() {
+		double[]coeficientes = {-5,3,0,-2,7,3,0};
+		Polinomio polinomio = new Polinomio(6, coeficientes);
+		//System.out.println("Programacion Dinamica Grado 6 " + polinomio.evaluarProgDinamica(3));
+		Assert.assertEquals(-2898, polinomio.evaluarProgDinamica(3), 0);
+	}
+	
+	@Test
+	public void programacionDinamicaGrado0() {
+		double[]coeficientes = {2};
+		Polinomio polinomio = new Polinomio(0, coeficientes);
+		//System.out.println("Programacion Dinamica Grado 0 " + polinomio.evaluarProgDinamica(2));
+		Assert.assertEquals(2, polinomio.evaluarProgDinamica(2), 0);
+	}
+	
+	@Test
+	public void programacionDinamicaGrado1() {
+		double[]coeficientes = {2,3};
+		Polinomio polinomio = new Polinomio(1, coeficientes);
+		//System.out.println("Programacion Dinamica Grado 1 " + polinomio.evaluarProgDinamica(2));
+		Assert.assertEquals(7, polinomio.evaluarProgDinamica(2), 0);
+	}
+	
+	@Test
+	public void mejoradaGrado3() {
+		double[]coeficientes = {2,-3,5,0};
+		Polinomio polinomio = new Polinomio(3, coeficientes);
+		//System.out.println("Mejorada Grado 3 " + polinomio.evaluarMejorada(3));
+		Assert.assertEquals(42, polinomio.evaluarMejorada(3), 0);
+	}
+	
+	@Test
+	public void mejoradaGrado6() {
+		double[]coeficientes = {-5,3,0,-2,7,3,0};
+		Polinomio polinomio = new Polinomio(6, coeficientes);
+		//System.out.println("Mejorada Grado 6 " + polinomio.evaluarMejorada(3));
+		Assert.assertEquals(-2898, polinomio.evaluarMejorada(3), 0);
+	}	
+	
+	
+	@Test
+	public void mejoradaGrado0() {
+		double[]coeficientes = {2};
+		Polinomio polinomio = new Polinomio(0, coeficientes);
+		//System.out.println("Mejorada Grado 0 " + polinomio.evaluarMejorada(2));
+		Assert.assertEquals(2, polinomio.evaluarMejorada(2), 0);
+	}
+	
+	@Test
+	public void mejoradaGrado1() {
+		double[]coeficientes = {2,3};
+		Polinomio polinomio = new Polinomio(1, coeficientes);
+		//System.out.println("Mejorada Grado 1 " + polinomio.evaluarMejorada(2));
+		Assert.assertEquals(7, polinomio.evaluarMejorada(2), 0);
+	}	
+	
+	@Test
+	public void evaluarPowGrado3() {
+		double[]coeficientes = {2,-3,5,0};
+		Polinomio polinomio = new Polinomio(3, coeficientes);
+		//System.out.println("Pow Grado 3 " + polinomio.evaluarPow(3));
+		Assert.assertEquals(42, polinomio.evaluarPow(3), 0);
+	}
+
+	@Test
+	public void evaluarPowGrado6() {
+		double[]coeficientes = {-5,3,0,-2,7,3,0};
+		Polinomio polinomio = new Polinomio(6, coeficientes);
+		//System.out.println("Pow Grado 6 " + polinomio.evaluarPow(3));
+		Assert.assertEquals(-2898, polinomio.evaluarPow(3), 0);
+	}
+	
+
+	@Test
+	public void evaluarPowGrado0() {
+		double[]coeficientes = {2};
+		Polinomio polinomio = new Polinomio(0, coeficientes);
+		//System.out.println("Pow Grado 0 " + polinomio.evaluarPow(2));
+		Assert.assertEquals(2, polinomio.evaluarPow(2), 0);
+	}
+	
+	@Test
+	public void evaluarPowGrado1() {
+		double[]coeficientes = {2,3};
+		Polinomio polinomio = new Polinomio(1, coeficientes);
+		//System.out.println("Pow Grado 1 " + polinomio.evaluarPow(2));
+		Assert.assertEquals(7, polinomio.evaluarPow(2), 0);
+	}	
+	
+	@Test
+	public void evaluarHornerGrado3() {
+		double[]coeficientes = {2,-3,5,0};
+		Polinomio polinomio = new Polinomio(3, coeficientes);
+		//System.out.println("Horner Grado 3 " + polinomio.evaluarHorner(3));
+		Assert.assertEquals(42, polinomio.evaluarHorner(3), 0);
+	}
+
+	@Test
+	public void evaluarHornerGrado6() {
+		double[]coeficientes = {-5,3,0,-2,7,3,0};
+		Polinomio polinomio = new Polinomio(6, coeficientes);
+		//System.out.println("Horner Grado 6 " + polinomio.evaluarHorner(3));
+		Assert.assertEquals(-2898, polinomio.evaluarHorner(3), 0);
+	}
+
+	@Test
+	public void evaluarHornerGrado0() {
+		double[]coeficientes = {5};
+		Polinomio polinomio = new Polinomio(0, coeficientes);
+		//System.out.println("Horner Grado 0 " + polinomio.evaluarHorner(3));
+		Assert.assertEquals(5, polinomio.evaluarHorner(3), 0);
+	}
+	
+	@Test
+	public void evaluarHornerGrado1() {
+		double[]coeficientes = {3,5};
+		Polinomio polinomio = new Polinomio(1, coeficientes);
+		//System.out.println("Horner Grado 1 " + polinomio.evaluarHorner(3));
+		Assert.assertEquals(14, polinomio.evaluarHorner(3), 0);
 	}
 
 }
--- src/io/Polinomio.java
+++ src/io/Polinomio.java
@@ -18,83 +18,98 @@ public double evaluarMSucesivas (double x) {
 
 	double result = 0, termino = 1;
 	int i = 0, j = 0;
+	
 	for(i = 0; i <= grado ; i++)
 	{
 		for(j = 0; j < i ; j++) 
 		{
 			termino *= x;
 		}
-		result += coeficientes[grado-i] * termino;
+		result += coeficientes[grado-i] * termino; //Sumo cada termino
 		termino = 1;
 	}
 	return result;
 }
  
-//public double evaluarRecursiva (double x) { //NO FUNCIONA
+//public double evaluarRecursiva (double x) {
 //	
-////a) Sin considerar si el exponente es par o impar: 
-////		potencia (x ,n)=x∗potencia (x ,n−1) 
-//
-//	return coeficientes[grado]*potencia(x, grado)+
-//		   coeficientes[grado-1]*potencia(x, grado-1);
+//a) Sin considerar si el exponente es par o impar: 
+//		potencia (x ,n)=x∗potencia (x ,n−1) 
 //}
 
 
-public double evaluarRecursiva (double x) {//NO FUNCIONA
- if (coeficientes[grado] == 0)
-   return coeficientes[grado];
-  else 
-	  return coeficientes[grado]* Math.pow(x,grado)+ evaluarRecursiva(x);
-}
-
-//double potencia( double base, int exponente) {//NO FUNCIONA
-//	if(grado == 0)
-//	{
-//		return 1;
-//	} 
-//	else
-//	{
-//		return base * potencia(base, exponente -1); // llamada recursiva
-//	} 
-//}
-
-//public double evaluarRecursivaPar (double x) {//NO FUNCIONA
-////	b) Considerando si el exponente es par o impar: 
-////	Si n es par: potencia (x ,n)=potencia (x∗x ,n/2) 
-////	Si n es impar potencia (x ,n)=x∗potencia (x ,n−1)
-//	if(grado % 2 != 0) {
-//	return result;
+//public double evaluarRecursivaParImpar (double x) {
+//	b) Considerando si el exponente es par o impar: 
+//	Si n es par: potencia (x ,n) = potencia (x∗x ,n/2) 
+//	Si n es impar potencia (x ,n) = x∗potencia (x ,n−1)
 //}
 
 public double evaluarProgDinamica (double x) {
 	double result = 0, termino = 1;
 	int i = 0;
 	
-	result += coeficientes[grado];
+	result += coeficientes[grado]; //sumo el termino independiente
 	for(i = grado-1; i >= 0 ; i--)
 	{
 		termino *= x; 
-		result += coeficientes[i] * termino;
+		result += coeficientes[i] * termino; //Sumo cada termino
 	}
 	return result;
 }
 
-//public double evaluarMejorada (double x) {
-//	return 0;
-//}
-//public double evaluarPow (double x) { //y a sugerencia de Lucas P 
-//	return 0;
-//}
-
-public double evaluarHorner ( double x) {//NO FUNCIONA
-	int i;
-	double resultado = 0;
-	for(i=0 ; i < grado; i++){
-		  //Multiplicar al valor parcial el valor de x más el coeficiente
-		  resultado = resultado * x + coeficientes[i];
+public double evaluarMejorada(double x) {
+		double res = 0;
+		double potencia = x;
+		
+		if(this.grado == 0)
+			return this.coeficientes[0];
+		
+		if(this.grado == 1)
+			return this.coeficientes[0]*x + this.coeficientes[1];
+		
+		//Sumo termino lineal e independiente
+		res += (x*this.coeficientes[this.coeficientes.length - 2] + this.coeficientes[this.coeficientes.length - 1]);
+		
+		//Empiezo a evaluar el polinomio desde el termino cuadratico hacia el mas alto.
+		for(int i = this.grado-2; i >= 0; i--) {
+			potencia *= x;
+			
+			res += potencia*this.coeficientes[i];
 		}
-	return resultado;
+		return res;
+	}
+	
+public double evaluarPow(double x) {		//Adapto la primer funcion de sumas sucesivas
+		double res = 0;
+		int gradoAux = this.grado;				//La utilizo para iterar en cada potencia.		
+		
+		if(this.grado == 0)
+			return this.coeficientes[0];
+		
+		if(this.grado == 1)
+			return this.coeficientes[0]*x + this.coeficientes[1];
+		
+		//Itero por cada potencia termino elevado al cuadrado o mayor en los cuales realizo multiplicaciones sucesivas.
+		for(int i = 0; i < this.grado-1; i++) {
+			double multSucesiva = Math.pow(x, gradoAux);
+			res += multSucesiva*this.coeficientes[i];
+			gradoAux--;
+		}
+		
+		//Una vez que tengo calculados todos los terminos elevados, le sumo el termino lineal multiplicado por el punto a analizar, mas el termino independiente.
+		res += (x*this.coeficientes[this.coeficientes.length - 2] + this.coeficientes[this.coeficientes.length - 1]);
+		
+		return res;
+	}
 
-}
-
+public double evaluarHorner(double x) {
+		double res = this.coeficientes[0];
+		
+		for(int i = 1; i < this.coeficientes.length; i++) {
+			res *= x;
+			res += this.coeficientes[i];
+		}
+		
+		return res;
+	}
 }
